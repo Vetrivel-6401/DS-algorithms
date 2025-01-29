a4fb414067d25f8b69914fb5a37bccc1753a5bdf
Vetrivel-6401 <vetrivel6401@gmail.com>
2025-01-29 21:07:16 +0530

    bubble sort

--- Algorithms/bubble_sort.py
+++ Algorithms/bubble_sort.py
@@ -1,18 +1,24 @@
-#Bubble Sort is an algorithm that sorts an array from the lowest value to the highest value.
+# To implement the Bubble Sort algorithm in a programming language, we need:
+
+# An array with values to sort.
+# An inner loop that goes through the array and swaps values if the first value is higher than the next value. This loop must loop through one less value each time it runs.
+# An outer loop that controls how many times the inner loop must run. For an array with n values, this outer loop must run n-1 times.
+
 
 def bubble_sort(arr):
-    n=len(arr)
+    size=len(arr)
 
-    for i in range(n-1):
-        for j in range(n-i-1):
+    for i in range(size-1):
+        swap=False
+        for j in range(size-i-1):
             if arr[j]>arr[j+1]:
-                arr[j],arr[j+1]=arr[j+1],arr[j]
+                arr[j+1],arr[j]=arr[j],arr[j+1]
+                swap=True
+        if not swap:
+            break
 
 
-    return arr
 
-
-
-arr=[11,13,2,1,44,55,33,22,5,7]
-sorted_array=bubble_sort(arr)
-print(sorted_array)
+arr=[4,3,5,6,7,8,18,99,112,34,55,23]
+bubble_sort(arr)
+print(arr)
